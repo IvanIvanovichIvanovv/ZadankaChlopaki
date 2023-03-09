ed26ca4717449e754763ddb6e31453527281acf1
IvanIvanovichIvanovv <dami0702@wp.pl>
2023-03-10 00:11:04 +0100

    popraweczki eksdi

--- Testy/Figury.cs
+++ Testy/Figury.cs
@@ -89,14 +89,14 @@ namespace Testy
         {
             int result = 0;
 
-            Assert.AreEqual(result, 0);
+            Assert.AreEqual(result, -1);
         }
         [TestMethod]
         public void Obwod_Kwadratu_m3() 
         {
             int result = 0;
 
-            Assert.AreEqual(result, 9);
+            Assert.AreEqual(result, 12);
         }
         [TestMethod]
         public void Obwod_Kwadratu_0()
