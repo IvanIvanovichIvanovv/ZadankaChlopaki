61bb51dfc35422e5d03632d7231f8641ba04f356
IvanIvanovichIvanovv <dami0702@wp.pl>
2023-03-10 02:04:32 +0100

    ustawienie nazw testow na szytwno - figury

--- Testy/Figury.cs
+++ Testy/Figury.cs
@@ -6,67 +6,67 @@ namespace Testy
     [TestClass]
     public class Figury
     {
-        static Program _program=new Program();
+        static Program _program = new Program();
         #region Metody
         [TestMethod]
         public void Pole_Kwadratu_4x4_metoda()
         {
-            int result = 0;
+            int result = Program.PoleKwadratu(4);
 
             Assert.AreEqual(16, result);
         }
         [TestMethod]
         public void Pole_Prostokata_6x9_metoda()
         {
-            int result = 0;
+            int result = Program.PoleProstokata(6, 9);
 
             Assert.AreEqual(54, result);
         }
         [TestMethod]
         public void Pole_Trojkata_2x8_metoda()
         {
-            int result = 0;
+            int result = Program.PoleTrojkata(2, 8);
 
             Assert.AreEqual(8, result);
         }
 
         [TestMethod]
-        public void Obwod_Kwadratu_4x4_Metoda() 
+        public void Obwod_Kwadratu_4x4_Metoda()
         {
-            int result = 0;
+            int result = Program.ObwodKwadratu(4);
 
-            Assert.AreEqual(16,result);
+            Assert.AreEqual(16, result);
         }
         [TestMethod]
         public void Obwod_Prostokata_6x9_Metoda()
         {
-            int result = 0;
+            int result = Program.ObwodProstokata(6, 9);
 
             Assert.AreEqual(30, result);
         }
         [TestMethod]
         public void Obwod_Trojkata_3x4x5_Metoda()
         {
-            int result = 0;
+            int result = Program.ObwodTrojkata(3, 4, 5);
 
             Assert.AreEqual(12, result);
         }
         [TestMethod]
-        public void Czy_Trojkat_Istnieje_3x4x5() 
+        public void Czy_Trojkat_Istnieje_3x4x5()
         {
-            bool result = false;
+            bool result = Program.TrojkatIstnieje(3, 4, 5);
 
             Assert.IsTrue(result);
         }
         [TestMethod]
         public void Czy_Trojkat_Istnieje_1x4x5()
         {
-            bool result = true;
+            bool result = Program.TrojkatIstnieje(1, 4, 5);
 
             Assert.IsFalse(result);
         }
         [TestMethod]
-        public void Czy_trojkat_ma_wieksze_pole_od_drugiego() 
+        public void Czy_trojkat_ma_wieksze_pole_od_drugiego()
         {
             int a1 = 5;
             int h1 = 6;
@@ -78,86 +78,86 @@ namespace Testy
             Assert.IsTrue(result);
         }
         [TestMethod]
-        public void Pole_Kwadratu_m2_metoda() 
+        public void Pole_Kwadratu_m2_metoda()
         {
-            int result = 0;
+            int result = Program.PoleKwadratu(-2);
 
             Assert.AreEqual(result, 4);
         }
         [TestMethod]
         public void Pole_Kwadratu_0_metoda()
         {
-            int result = 0;
+            int result = Program.PoleKwadratu(0);
 
             Assert.AreEqual(result, -1);
         }
         [TestMethod]
-        public void Obwod_Kwadratu_m3() 
+        public void Obwod_Kwadratu_m3()
         {
-            int result = 0;
+            int result = Program.ObwodKwadratu(-3);
 
             Assert.AreEqual(result, 12);
         }
         [TestMethod]
         public void Obwod_Kwadratu_0()
         {
-            int result = 0;
+            int result = Program.ObwodKwadratu(0);
 
             Assert.AreEqual(result, -1);
         }
         [TestMethod]
         public void Pole_Prostokata_m2_5_metoda()
         {
-            int result = 0;
+            int result = Program.PoleProstokata(-2, 5);
 
             Assert.AreEqual(result, 10);
         }
         [TestMethod]
         public void Pole_Prostokata_m2_0_metoda()
         {
-            int result = 0;
+            int result = Program.PoleProstokata(-2, 0);
 
             Assert.AreEqual(result, -1);
         }
         [TestMethod]
-        public void Obwod_Prostokata_m2_5() 
+        public void Obwod_Prostokata_m2_5()
         {
-            int result = 0;
+            int result = Program.ObwodProstokata(-2, 5);
 
             Assert.AreEqual(result, 14);
         }
         [TestMethod]
         public void Obwod_Prostokata_0_5()
         {
-            int result = 0;
+            int result = Program.ObwodProstokata(0, 5);
 
             Assert.AreEqual(result, -1);
         }
         [TestMethod]
-        public void Pole_Trojkata_m2_4() 
+        public void Pole_Trojkata_m2_4()
         {
-            int result = 0;
+            int result = Program.PoleTrojkata(-2, 4);
 
             Assert.AreEqual(result, 4);
         }
         [TestMethod]
         public void Pole_Trojkata_0_4()
         {
-            int result = 0;
+            int result = Program.PoleTrojkata(0, 4);
 
             Assert.AreEqual(result, -1);
         }
         [TestMethod]
-        public void Obwod_Trojkata_3_m4_5() 
+        public void Obwod_Trojkata_3_m4_5()
         {
-            int result = 0;
+            int result = Program.ObwodTrojkata(3, -4, 5);
 
             Assert.AreEqual(result, 12);
         }
         [TestMethod]
         public void Obwod_Trojkata_3_0_5()
         {
-            int result = 0;
+            int result = Program.ObwodTrojkata(3, 0, 5);
 
             Assert.AreEqual(result, -1);
         }
