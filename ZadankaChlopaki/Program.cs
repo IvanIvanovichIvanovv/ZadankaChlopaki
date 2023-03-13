da6a4067c76878dcfcb213299d091d144e5f6d0f
Rudzioszek <filip.wilhelm99@gmail.com>
2023-03-13 19:57:48 +0100

    ccc

--- ZadankaChlopaki/Program.cs
+++ ZadankaChlopaki/Program.cs
@@ -1,13 +1,48 @@
-﻿using System;
-
-namespace ZadankaChlopaki // Note: actual namespace depends on the project name.
+﻿namespace ZadankaChlopaki // Note: actual namespace depends on the project name.
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-
             Console.ReadLine();
+        }
+        public static int PoleKwadratu(int a)
+        {
+            return a * a;
+        }
+
+        public static int PoleProstokata(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int PoleTrojkata(int a, int b)
+        {
+            return a * b / 2;
+        }
+
+        public static int ObwodKwadratu(int a)
+        {
+            return 4 * a;
+        }
+
+        public static int ObwodProstokata(int a, int b)
+        {
+            return 2 * (a + b);
+        }
+
+        public static int ObwodTrojkata(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public static bool CzyTrojkatIstnieje(int a, int b, int c)
+        {
+            if (a + b <= c || b + c <= a || c + a <= b)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
