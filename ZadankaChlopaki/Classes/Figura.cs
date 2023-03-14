d9cc32e1e25e16fe8244b9acb1f9c8c0021339c1
IvanIvanovichIvanovv <dami0702@wp.pl>
2023-03-14 15:47:08 +0100

    dodano nowe klasy do zabawy

---
+++ ZadankaChlopaki/Classes/Figura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadankaChlopaki.Classes
+{
+    public abstract class Figura
+    {
+        protected int a;
+        protected int h;
+
+        public abstract int Pole();
+        public abstract int Obwod();
+        public Figura(int a, int b )
+        {
+            this.a = a;
+            this.h = b;
+        }
+    }
+}
--- ZadankaChlopaki/Program.cs
+++ ZadankaChlopaki/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ZadankaChlopaki.Classes;
 
 namespace ZadankaChlopaki // Note: actual namespace depends on the project name.
 {
@@ -6,7 +7,8 @@ namespace ZadankaChlopaki // Note: actual namespace depends on the project name.
     {
         public static void Main(string[] args)
         {
-
+            Trojkat trojkat = new Trojkat(5, 4);
+            Console.WriteLine(trojkat.Pole());
             Console.ReadLine();
         }
     }
