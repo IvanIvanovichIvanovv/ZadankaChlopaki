8ac537f8066612c162be15c7687a1ad71b53a1ee
IvanIvanovichIvanovv <dami0702@wp.pl>
2023-03-15 16:00:43 +0100

    Rework struktury testow

---
+++ Testy/Figury/Klasy/KwadratKlasa.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZadankaChlopaki;
+using ZadankaChlopaki.Classes;
+
+namespace Testy
+{
+    [TestClass]
+    public class KwadratKlasa
+    {
+        static Program _program = new Program();
+        [TestMethod]
+        public void Pole_Kwadratu_4x4_klasa()
+        {
+            Kwadrat kwadrat = new Kwadrat(4);
+            int result = kwadrat.Pole();
+
+            Assert.AreEqual(16, result);
+        }
+        [TestMethod]
+        public void Pole_Kwadratu_m4xm4_klasa()
+        {
+            Kwadrat kwadrat = new Kwadrat(-4);
+            int result = kwadrat.Pole();
+
+            Assert.AreEqual(16, result);
+        }
+        [TestMethod]
+        public void Obwod_Kwadratu_4x4_klasa()
+        {
+            Kwadrat kwadrat = new Kwadrat(4);
+            int result = kwadrat.Obwod();
+
+            Assert.AreEqual(16, result);
+        }
+        [TestMethod]
+        public void Obwod_Kwadratu_m4xm4_klasa()
+        {
+            Kwadrat kwadrat = new Kwadrat(-4);
+            int result = kwadrat.Obwod();
+
+            Assert.AreEqual(16, result);
+        }
+    }
+}
---
+++ Testy/Figury/Klasy/TrojkatKlasa.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZadankaChlopaki;
+using ZadankaChlopaki.Classes;
+
+namespace Testy
+{
+    [TestClass]
+    public class TrojkatKlasa
+    {
+        static Program _program = new Program();
+        [TestMethod]
+        public void Pole_Trojkata_2x8_klasa()
+        {
+            Trojkat trojkat = new Trojkat(2, 8);
+            int result = trojkat.Pole();
+
+            Assert.AreEqual(8, result);
+        }
+        [TestMethod]
+        public void Pole_Trojkata_m2x8_klasa()
+        {
+            Trojkat trojkat = new Trojkat(2, 8);
+            int result = trojkat.Pole();
+
+            Assert.AreEqual(8, result);
+        }
+        [TestMethod]
+        public void Obwod_Trojkata_3x4x5_klasa()
+        {
+            Trojkat trojkat = new Trojkat(3, 4, 5);
+            int result = trojkat.Obwod();
+
+            Assert.AreEqual(12, result);
+        }
+        [TestMethod]
+        public void Obwod_Trojkata_m3x4x5_klasa()
+        {
+            Trojkat trojkat = new Trojkat(-3, 4, 5);
+            int result = trojkat.Obwod();
+
+            Assert.AreEqual(12, result);
+        }
+    }
+}
