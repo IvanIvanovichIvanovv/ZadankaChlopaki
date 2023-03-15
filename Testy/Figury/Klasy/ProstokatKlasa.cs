88738c41949269f094ce4082a0fc1b3e07ecbc28
IvanIvanovichIvanovv <dami0702@wp.pl>
2023-03-15 19:15:22 +0100

    Dodano 4 testy do prostokat/klasa

--- Testy/Figury/Klasy/ProstokatKlasa.cs
+++ Testy/Figury/Klasy/ProstokatKlasa.cs
@@ -61,5 +61,65 @@ namespace Testy
 
             CollectionAssert.AreEqual(new int[2] { 4, 6 }, result);
         }
+        [TestMethod]
+        public void Zmien_rozmiary_prostokata_na_4_6() 
+        {
+            Prostokat prostokat = new Prostokat(2, 4);
+            prostokat.ChangeDimensions(4, 6);
+
+            CollectionAssert.AreEqual(new int[2] { 4, 6 }, prostokat.GetDimensions());
+        }
+        [TestMethod]
+        public void Zmien_rozmiary_prostokata_na_m4_m6()
+        {
+            Prostokat prostokat = new Prostokat(2, 4);
+            prostokat.ChangeDimensions(-4, -6);
+
+            CollectionAssert.AreEqual(new int[2] { 4, 6 }, prostokat.GetDimensions());
+        }
+        [TestMethod]
+        public void Zmien_dlugosc_pierwszego_boku_na_6() 
+        {
+            Prostokat prostokat=new Prostokat(2,3);
+
+            prostokat.ChangeA(6);
+
+            int[] result = prostokat.GetDimensions();
+
+            Assert.AreEqual(6, result[0]);
+        }
+        [TestMethod]
+        public void Zmien_dlugosc_pierwszego_boku_na_m6()
+        {
+            Prostokat prostokat = new Prostokat(2, 3);
+
+            prostokat.ChangeA(-6);
+
+            int[] result = prostokat.GetDimensions();
+
+            Assert.AreEqual(6, result[0]);
+        }
+        [TestMethod]
+        public void Zmien_dlugosc_drugiego_boku_na_m6()
+        {
+            Prostokat prostokat = new Prostokat(2, 3);
+
+            prostokat.ChangeB(-6);
+
+            int[] result = prostokat.GetDimensions();
+
+            Assert.AreEqual(6, result[1]);
+        }
+        [TestMethod]
+        public void Zmien_dlugosc_drugiego_boku_na_6()
+        {
+            Prostokat prostokat = new Prostokat(2, 3);
+
+            prostokat.ChangeB(6);
+
+            int[] result = prostokat.GetDimensions();
+
+            Assert.AreEqual(6, result[1]);
+        }
     }
 }
