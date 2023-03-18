324c3c700577d3cb9481f6cc66f78c2e0bc638eb
Rudzioszek <filip.wilhelm99@gmail.com>
2023-03-18 21:34:22 +0100

    trójkąt funkcje redi

--- Testy/Figury/Funkcje/TrojkatFunkcje.cs
+++ Testy/Figury/Funkcje/TrojkatFunkcje.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ZadankaChlopaki;
-using ZadankaChlopaki.Classes;
-/*
+
 namespace Testy
 {
     [TestClass]
@@ -81,7 +75,6 @@ namespace Testy
 
             Assert.AreEqual(result, -1);
         }
-        
+
     }
 }
-*/
--- ZadankaChlopaki/Program.cs
+++ ZadankaChlopaki/Program.cs
@@ -38,7 +38,20 @@ namespace ZadankaChlopaki // Note: actual namespace depends on the project name.
 
         public static int PoleTrojkata(int a, int b)
         {
-            return a * b / 2;
+            if (a == 0 || b == 0)
+            {
+                return -1;
+            }
+            if (a < 0)
+            {
+                return -a * b/2;
+            }
+            if (b < 0)
+            {
+                return -b * a/2;
+            }
+            else
+                return a * b/2;
         }
 
         public static int ObwodKwadratu(int a)
@@ -74,16 +87,41 @@ namespace ZadankaChlopaki // Note: actual namespace depends on the project name.
 
         public static int ObwodTrojkata(int a, int b, int c)
         {
-            return a + b + c;
+            if (a == 0 || b == 0)
+            {
+                return -1;
+            }
+            if (a < 0)
+            {
+                return -a + b + c;
+            }
+            if (b < 0)
+            {
+                return a + -b + c;
+            }
+            if (c < 0)
+            {
+                return a + b + -c;
+            }
+            return 2 * (a + b);
         }
 
-        public static bool CzyTrojkatIstnieje(int a, int b, int c)
+        public static bool TrojkatIstnieje(int a, int b, int c)
         {
             if (a + b <= c || b + c <= a || c + a <= b)
             {
                 return false;
             }
             return true;
+        }
+
+        public static bool Czy_pierwszy_wiekszy_od_drugiego(int a1, int h1, int a2, int h2)
+        {
+            if (a1 * h1 > a2 *h2)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
